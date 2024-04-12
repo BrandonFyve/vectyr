be1685b4e6744764c58adf66415346b60ad49f9d
BrandonFyve <70907495+BrandonFyve@users.noreply.github.com>
2024-04-12 01:17:44 +0100

    Fix error handling in data-points endpoint

--- src/app/api/data-points/route.ts
+++ src/app/api/data-points/route.ts
@@ -10,46 +10,53 @@ interface DataPoint {
 }
 
 export async function POST(request: NextRequest) {
-  const ai = new Ai(getRequestContext().env.AI);
+  try {
+    const ai = new Ai(getRequestContext().env.AI);
 
-  const body: DataPoint = await request.json();
+    const body: DataPoint = await request.json();
 
-  const { text } = body;
-  if (!text) {
-    return NextResponse.json({ error: 'Missing text' }, { status: 400 });
+    const { text } = body;
+    if (!text) {
+      return NextResponse.json({ error: 'Missing text' }, { status: 400 });
+    }
+
+    const { results } = await getRequestContext()
+      .env.DB.prepare('INSERT INTO datapoints (text) VALUES (?) RETURNING *')
+      .bind(text)
+      .all<DataPoint>();
+
+    const record = results.length ? results[0] : null;
+
+    if (!record) {
+      return NextResponse.json(
+        { error: 'Failed to create note' },
+        { status: 500 }
+      );
+    }
+
+    const { data } = await ai.run('@cf/baai/bge-base-en-v1.5', {
+      text: [text],
+    });
+    const values = data[0];
+
+    if (!values) {
+      return NextResponse.json(
+        { error: 'Failed to generate vector embedding' },
+        { status: 500 }
+      );
+    }
+
+    const { id } = record;
+    const inserted = await getRequestContext().env.VECTOR_INDEX.upsert([
+      {
+        id: id.toString(),
+        values,
+      },
+    ]);
+
+    return NextResponse.json({ id, text });
+  } catch (error: any) {
+    console.log(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
-
-  const { results } = await getRequestContext()
-    .env.DB.prepare('INSERT INTO datapoints (text) VALUES (?) RETURNING *')
-    .bind(text)
-    .all<DataPoint>();
-
-  const record = results.length ? results[0] : null;
-
-  if (!record) {
-    return NextResponse.json(
-      { error: 'Failed to create note' },
-      { status: 500 }
-    );
-  }
-
-  const { data } = await ai.run('@cf/baai/bge-base-en-v1.5', { text: [text] });
-  const values = data[0];
-
-  if (!values) {
-    return NextResponse.json(
-      { error: 'Failed to generate vector embedding' },
-      { status: 500 }
-    );
-  }
-
-  const { id } = record;
-  const inserted = await getRequestContext().env.VECTOR_INDEX.upsert([
-    {
-      id: id.toString(),
-      values,
-    },
-  ]);
-
-  return NextResponse.json({ id, text });
 }
